9387d426e836c974f75ea65418c593520056b143
DmitryLotkov <dmitry.lotkov9@gmail.com>
2022-03-29 22:25:01 +0300

    2. Added scroll animation, correct media request

--- src/Main/SideBar/SideBar.tsx
+++ src/Main/SideBar/SideBar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import style from "./SideBar.module.css";
 import {NameAndProf} from "./NameAndProf";
 import Avatar from "./MyPhoto.jpg"
@@ -9,12 +9,25 @@ import {faArrowAltCircleDown} from "@fortawesome/free-regular-svg-icons";
 
 
 export function SideBar() {
+    const [scroll, setScroll] = useState<number>(0);
+
+    useEffect(() => {
+        document.addEventListener("scroll", scrollHandler);
+        return () => {
+            document.addEventListener("scroll", scrollHandler);
+        }
+    }, [])
+    const scrollHandler = (e: any) => {
+        setScroll(e.target.documentElement.scrollTop);
+    }
+
 
     return (
         <aside className={style.aside}>
             <div className={style.slideBlock}>
                 <div className={style.nameAvatar}>
-                    <img className={style.avatar} src={Avatar} alt={"avatar"}/>
+                    <img className={scroll <= 80 ? style.avatar : style.avatarActive}
+                         src={Avatar} alt={"avatar"}/>
                     <NameAndProf/>
                 </div>
                 <ContactBlock/>
