6880e48730e3b2709b97a979fc454b20d302da0d
DmitryLotkov <dmitry.lotkov9@gmail.com>
2022-08-07 22:33:48 +0300

    9. Added hover effect on my works container + link button

--- src/Main/MainContent/Works/WorksItem.tsx
+++ src/Main/MainContent/Works/WorksItem.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import style from "./Works.module.scss"
 
+
 type WorksItemPropsType = {
     description: string
     title: string
@@ -10,16 +11,15 @@ type WorksItemPropsType = {
 
 export function WorksItem(props: WorksItemPropsType) {
     return (
-        <a className={style.worksItem} href={props.href}>
-
-                <img className={style.img} src={props.src} alt={"work Img"}/>
-                <div className={style.titleContent}>
-                    <h3>{props.title}</h3>
-                    <p>
-                        {props.description}
-                    </p>
-                </div>
-
-        </a>
+        <div className={style.worksItem}>
+            <img className={style.img} src={props.src} alt={"work Img"}/>
+            <div className={style.titleContent}>
+                <h3>{props.title}</h3>
+                <p>
+                    {props.description}
+                </p>
+            </div>
+            <a href={props.href} className={style.showLink}>Show</a>
+        </div>
     );
 }
