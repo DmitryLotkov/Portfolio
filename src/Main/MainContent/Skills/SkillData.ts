e60e1239d02ce6e43cec0e6220db077c674a3fec
DmitryLotkov <dmitry.lotkov9@gmail.com>
2022-08-08 22:31:20 +0300

    11. Changed skills logos to svg format

--- src/Main/MainContent/Skills/SkillData.ts
+++ src/Main/MainContent/Skills/SkillData.ts
@@ -1,14 +1,13 @@
 import {faJsSquare} from "@fortawesome/free-brands-svg-icons";
-import {faReact} from "@fortawesome/free-brands-svg-icons";
 import {faHtml5} from "@fortawesome/free-brands-svg-icons";
-// import {faCss3} from "@fortawesome/free-brands-svg-icons";
-import {faCode} from "@fortawesome/free-solid-svg-icons/faCode";
-import tsLogo from "./../../../Images/typescript.svg";
 import ReduxLogo from "./../../../Images/Redux.svg";
 import StorybookLogo from "./../../../Images/Storybook.svg";
 import SassLogo from "./../../../Images/Sass_Logo_Color.svg";
 import MuiLogo from "./../../../Images/mui-logo.svg";
 import RestAPI from "./../../../Images/rest.svg";
+import ReactLogo from "./../../../Images/ReactLogo.svg";
+import JestLogo from "./../../../Images/jest-svgrepo-com.svg";
+import TSLogo from "./../../../Images/typescript-svgrepo-com.svg";
 
 type DataType = {
     picture: any,
@@ -24,7 +23,7 @@ export const skillsData:skillsDataType = {
 
     data: [
         {
-            picture: faReact,
+            picture: ReactLogo,
             skill: "React JS",
             skillDescription: "A year of experience using React JS hooks to create SPA applications. Familiarity to React class components",
         },
@@ -39,7 +38,7 @@ export const skillsData:skillsDataType = {
             skillDescription: "Confident knowledge of native JavaScript (ES6+/ events/ Promises/ functional programming/ prototypes). Familiarity to classes",
         },
         {
-            picture: tsLogo,
+            picture: TSLogo,
             skill: "TS",
             skillDescription: "A year of practical experience using TypeScript for developing React SPA apps (types, interface, enum). Familiarity to classes",
         },
@@ -54,7 +53,7 @@ export const skillsData:skillsDataType = {
             skillDescription: "Using the REST API as the main technology for network requests. Practical experience of using Postman for server testing",
         },
         {
-            picture: faCode,
+            picture: JestLogo,
             skill: "Unit testing",
             skillDescription: "Test Driven Development (TDD) approach, unit testing using Jest and React testing library",
         },
